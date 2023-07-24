e5bf1755f65c6200c396f5dfe137bc8a94b6a750
John Enad <jenad88@hotmail.com>
2023-07-23 21:38:24 -0400

    add state management using prop drilling approach

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,31 +1,63 @@
 import { NavLink, Link, useSearchParams, Form } from 'react-router-dom';
 import logo from './logo.svg';
 
-export function Header() {
+import { User } from './api/authenticate';
+
+type Props = {
+  user: undefined | User;
+  onSignInClick: () => void;
+  loading: boolean;
+};
+
+export function Header({ user, onSignInClick, loading }: Props) {
   const [searchParams] = useSearchParams();
 
   return (
-    <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
-      <Form className="relative text-right" action="/products">
-        <input
-          type="search"
-          name="search"
-          placeholder="Search"
-          defaultValue={searchParams.get('search') ?? ''}
-          className="absolute right-0 top-0 rounded py-2 px-3         text-gray-700"
-        />
-      </Form>
-      <Link to="">
-        <img src={logo} alt="Logo" className="inline-block h-20" />
-      </Link>
-      <Link to="">
-        <h1 className="text-2xl">React Tools</h1>
-      </Link>
+    <header className="flex flex-col border-b-2 border-gray-400 pb-6 h-28 bg-gray-900">
+      <div className="flex flex-row justify-between align-baseline py-2">
+        <div className="flex flex-row align-baseline">
+          <Link to="">
+            <img src={logo} alt="Logo" className="h-12" />
+          </Link>
+          <Link to="" className="flex items-center">
+            <h1 className="text-2x text-white">React Tools</h1>
+          </Link>
+        </div>
+        <div className="flex flex-row align-baseline">
+          <div>
+            <Form className="text-right" action="/products">
+              <input
+                type="search"
+                name="search"
+                placeholder="Search"
+                defaultValue={searchParams.get('search') ?? ''}
+                className="rounded py-2 px-3 text-gray-700"
+              />
+            </Form>
+          </div>
+          <div>
+            {user ? (
+              <span className="ml-auto font-bold">{user.name} has signed in</span>
+            ) : (
+              <button
+                onClick={onSignInClick}
+                className="whitespace-nowrap inline-flex items-center justify-center ml-2 
+                px-4 py-2 w-36 border border-transparent rounded-md shadow-sm 
+                text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                disabled={loading}
+              >
+                {loading ? '...' : 'Sign in'}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+
       <nav>
         <NavLink
           to="products"
           className={({ isActive }) =>
-            `text-white no-underline p-1 pb-0.5 border-solid        border-b-2 ${
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
               isActive ? 'border-white' : 'border-transparent'
             }`
           }
@@ -35,7 +67,7 @@ export function Header() {
         <NavLink
           to="contact-us"
           className={({ isActive }) =>
-            `text-white no-underline p-1 pb-0.5 border-solid        border-b-2 ${
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
               isActive ? 'border-white' : 'border-transparent'
             }`
           }
@@ -45,7 +77,7 @@ export function Header() {
         <NavLink
           to="contact-us2"
           className={({ isActive }) =>
-            `text-white no-underline p-1 pb-0.5 border-solid        border-b-2 ${
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
               isActive ? 'border-white' : 'border-transparent'
             }`
           }
@@ -55,7 +87,7 @@ export function Header() {
         <NavLink
           to="contact-us3"
           className={({ isActive }) =>
-            `text-white no-underline p-1 pb-0.5 border-solid        border-b-2 ${
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
               isActive ? 'border-white' : 'border-transparent'
             }`
           }
@@ -74,7 +106,7 @@ export function Header() {
         <NavLink
           to="personscore"
           className={({ isActive }) =>
-            `text-white no-underline p-1 pb-0.5 border-solid        border-b-2 ${
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
               isActive ? 'border-white' : 'border-transparent'
             }`
           }
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -6,7 +6,7 @@ export function ErrorPage() {
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="text-center p-5 text-xl">
         <h1 className="text-xl text-slate-900">Sorry, an error has occurred</h1>
         {isError(error) && <p className="text-base text-slate-700">{error.statusText}</p>}
