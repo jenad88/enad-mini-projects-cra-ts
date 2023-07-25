5888b2db7bc0a196af0be7b62685f7a5b692b927
John Enad <jenad88@hotmail.com>
2023-07-24 20:54:07 -0400

    Use react-query sample

--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,30 +1,47 @@
 import { Suspense } from 'react';
 import { useLoaderData, Await } from 'react-router-dom';
-import { assertIsPosts } from './getPosts';
-import { PostData, NewPostData } from './types';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { assertIsPosts, getPosts } from './getPosts';
+import { PostData } from './types';
 import { PostsList } from './PostsList';
 import { savePost } from './savePost';
 import { NewPostForm } from './NewPostForm';
 
 export function PostsPage() {
-  const data = useLoaderData();
-  assertIsData(data);
-  async function handleSave(newPostData: NewPostData) {
-    await savePost(newPostData);
-  }
+  const { isLoading, data: posts } = useQuery(['postsData'], getPosts);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(savePost, {
+    onSuccess: (savedPost) => {
+      queryClient.setQueryData<PostData[]>(['postsData'], (oldPosts) => {
+        if (oldPosts === undefined) {
+          return [savedPost];
+        } else {
+          return [savedPost, ...oldPosts];
+        }
+      });
+    },
+  });
 
+  //   const data = useLoaderData();
+  //   assertIsData(data);
+
+  if (isLoading || posts === undefined) {
+    return <div className="w-96 mx-auto mt-6">Loading ...</div>;
+  }
   return (
     <div className="w-96 mx-auto mt-6">
       <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
-      <NewPostForm onSave={handleSave} />
-      <Suspense fallback={<div>Fetching...</div>}>
+      <NewPostForm onSave={mutate} />
+      <PostsList posts={posts} />
+
+      {/* <Suspense fallback={<div>Fetching...</div>}>
         <Await resolve={data.posts}>
           {(posts) => {
             assertIsPosts(posts);
             return <PostsList posts={posts} />;
           }}
         </Await>
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 }
